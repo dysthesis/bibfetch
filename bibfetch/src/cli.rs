3250d31013dfc92f206176b00bd04dfa28223b4a
Dysthesis <antheoraviel@protonmail.com>
2025-04-20 11:51:31 +1000

    Clean up unused imports

--- bibfetch/src/cli.rs
+++ bibfetch/src/cli.rs
@@ -12,6 +12,7 @@ pub struct Args {
     pub identifiers: Vec<String>,
     pub handler: Option<String>,
     pub handlers_path: Option<String>,
+    pub plugins_path: Option<String>,
 }
 
 impl Args {
@@ -24,6 +25,7 @@ impl Args {
         let mut identifiers = None;
         let mut handler = None;
         let mut handlers_path = None;
+        let mut plugins_path = None;
 
         // Keep on matching on the command line arguments to parse it
         let mut parser = lexopt::Parser::from_env();
@@ -35,6 +37,10 @@ impl Args {
 
                 Short('H') | Long("handler") => {
                     handler = Some(parser.value()?.parse()?);
+                }
+
+                Short('P') | Long("plugins-path") => {
+                    plugins_path = Some(parser.value()?.parse::<String>()?);
                 }
 
                 Value(val) if identifiers.is_none() => {
@@ -49,6 +55,7 @@ impl Args {
             identifiers: identifiers.ok_or(anyhow!("Missing argument IDENTIFIERS!"))?,
             handler,
             handlers_path,
+            plugins_path,
         })
     }
 }
